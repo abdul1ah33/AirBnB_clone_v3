96e8cec97160057beec846eb76c2392e121ec379
Sheelah <sheelah.mogaka@gmail.com>
2024-06-11 04:43:45 -0700

    Updated api.py.

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -7,6 +7,8 @@ from flask import Blueprint
 
 app_views = Blueprint('/api/v1', __name__, url_prefix="/api/v1")
 
+auth_blueprint = Blueprint('auth', __name__)
+
 from api.v1.views.index import *
 from api.v1.views.states import *
 from api.v1.views.amenities import *
@@ -15,3 +17,4 @@ from api.v1.views.places import *
 from api.v1.views.places_reviews import *
 from api.v1.views.users import *
 from api.v1.views.places_amenities import *
+
