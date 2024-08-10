70a61a60ffedfa2d9ab55c925953d6bdf2c7f603
abdul1ah33 <study.abdullah2005@gmail.com>
2024-08-10 08:03:57 +0000

    d

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 """index"""
-form flask import jsonify
+from flask import jsonify
 from api.v1.views import app_views
 
-@app_views_route('/status')
+@app_views.route('/status')
 def api_status():
-""" Status of API """
-response = {"status": "OK"}
-return jsonify(response)
+	""" Status of API """
+	response = {"status": "OK"}
+	return jsonify(response)
