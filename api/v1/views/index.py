888a3139d56b90606ce9058642f9f59648a9b6da
abdul1ah33 <study.abdullah2005@gmail.com>
2024-08-10 08:34:05 +0000

    df

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -9,3 +9,16 @@ def api_status():
     """ Status of API """
     response = {"status": "OK"}
     return jsonify(response)
+
+
+@app_views.route('/stats')
+def api_stats():
+    """ Retrieves the number of each objects by type """
+    classes = [Amenity, City, Place, Review, State, User]
+    names = ["amenities", "cities", "places", "reviews", "states", "users"]
+
+    num_objs = {}
+    for i in range(len(classes)):
+        num_objs[names[i]] = storage.count(classes[i])
+
+    return jsonify(num_objs)
