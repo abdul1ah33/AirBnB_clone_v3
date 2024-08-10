522f376a7e67dcd0c8a78bf3e52ea7a980057c7b
abdul1ah33 <study.abdullah2005@gmail.com>
2024-08-10 20:25:10 +0000

    d

--- api/v1/app.py
+++ api/v1/app.py
@@ -2,11 +2,22 @@
 """ Flask Application """
 from os import getenv
 from models import storage
-from flask import Flask
+from flask import Flask, jsonify, make_response
 from api.v1.views import app_views
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
+
+
+@app.errorhandler(404)
+def not_found(error):
+    """ 404 Error
+    ---
+    responses:
+      404:
+        description: a resource was not found
+    """
+    return make_response(jsonify({'error': "Not found"}), 404)
 
 
 @app.teardown_appcontext
