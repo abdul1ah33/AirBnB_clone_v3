dda85fb627568874b4e128d5e8ac2a924375d0ac
abdul1ah33 <study.abdullah2005@gmail.com>
2024-08-10 08:27:18 +0000

    df

--- api/v1/app.py
+++ api/v1/app.py
@@ -9,6 +9,12 @@ app = Flask(__name__)
 app.register_blueprint(app_views)
 
 
+@app.teardown_appcontext
+def close_db(error):
+    """ Close Storage """
+    storage.close()
+
+
 if __name__ == "__main__":
     """ Main Function """
     HOST = getenv('HBNB_API_HOST', '0.0.0.0')
