ea97ec7690845e71b88b84eb06b25219283d2858
abdul1ah33 <study.abdullah2005@gmail.com>
2024-08-10 07:54:59 +0000

    api task 3

---
+++ api/v1/app.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" Flask Application """
+from os import getenv
+from models import storage
+from flask import Flask
+from api.v1.views import app_views
+
+app = Flask(__name__)
+app.register_blueprint(app_views)
+
+
+if __name__ == "__main__":
+	""" Main Function """
+	HOST = getenv('HBNB_API_HOST', '0.0.0.0')
+	PORT = getenv('HBNB_API_PORT', 5000)
+	app.run(host=HOST, port=PORT, threaded=True)
---
+++ api/v1/views/__init__.py
@@ -0,0 +1,7 @@
+#/!usr/bin/python3
+"""Blueprint for PI """
+from flask import Blueprint
+
+app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
+
+form api.views.index import *
